32f213b9617cad7f80f1086d26464a907d8ce1ab
Joanna Murdzheva <joanna.murdzheva@icb.bg>
2019-06-27 13:00:11 +0300

    Add company component.

--- SBUI/src/app/app.module.ts
+++ SBUI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company/company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,

3e0b04e5454b2dfa4c5c0fba2ebd9259e47dd5c0
Joanna Murdzheva <joanna.murdzheva@icb.bg>
2019-07-01 15:02:03 +0300

    Structure change

--- SBUI/src/app/app-routing.module.ts
+++ SBUI/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CompanyComponent } from './components/company/company.component';
-import { OverviewComponent } from './components/overview/overview.component';
-import { ControlsComponent } from './components/controls/controls.component';
-import { StructureComponent } from './components/company/structure/structure.component';
-import { ContactsComponent } from './components/company/contacts/contacts.component';
+import { OverviewComponent } from './overview/overview.component';
+import { CompanyComponent } from './company/company.component';
+import { StructureComponent } from './company/structure/structure.component';
+import { ContactsComponent } from './company/contacts/contacts.component';
+import { ControlsComponent } from './controls/controls.component';
 
 const routes: Routes = [ 
   { path: 'overview', component: OverviewComponent },
@@ -13,7 +13,6 @@ const routes: Routes = [
     children: [
       { path: 'structure', component: StructureComponent },
       { path: 'contacts', component: ContactsComponent },
-      { path: 'overview', component: OverviewComponent },
     ]
   },
   { path: 'controls', component: ControlsComponent },
--- SBUI/src/app/app.module.ts
+++ SBUI/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CompanyComponent } from './components/company/company.component';
-import { OverviewComponent } from './components/overview/overview.component';
-import { ControlsComponent } from './components/controls/controls.component';
-import { CompanyDetailsComponent } from './components/company/company-details/company-details.component';
-import { WizardComponent } from './components/company/wizard/wizard.component';
-import { StructureComponent } from './components/company/structure/structure.component';
-import { ContactsComponent } from './components/company/contacts/contacts.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ControlsComponent } from './controls/controls.component';
+import { ContactsComponent } from './company/contacts/contacts.component';
+import { CompanyDetailsComponent } from './company/company-details/company-details.component';
+import { WizardComponent } from './company/wizard/wizard.component';
+import { StructureComponent } from './company/structure/structure.component';
+import { CompanyComponent } from './company/company.component';
 
 @NgModule({
   declarations: [

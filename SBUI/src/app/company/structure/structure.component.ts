310881f3a9424f3c13c4d02fb52ce619cd834105
Joanna Murdzheva <joanna.murdzheva@icb.bg>
2019-07-04 14:45:14 +0300

    Remove unused code.

--- SBUI/src/app/company/structure/structure.component.ts
+++ SBUI/src/app/company/structure/structure.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { Type } from 'src/app/models/type';
 import { Object } from 'src/app/models/object';
 import { Tfm } from 'src/app/models/tfm';
-import { ComboBoxComponent, AutoCompleteComponent } from '@progress/kendo-angular-dropdowns';
+import { ComboBoxComponent, AutoCompleteComponent, PopupComponent } from '@progress/kendo-angular-dropdowns';
 
 @Component({
   selector: 'app-structure',
@@ -13,24 +13,25 @@ import { ComboBoxComponent, AutoCompleteComponent } from '@progress/kendo-angula
 export class StructureComponent {
 
   public types: Type[];
+  @ViewChild('typesCombobox') typesCombobox: ComboBoxComponent;
 
   public objects: Object[];
   public selectedObjects: any[] = [];
   public showObjects: boolean = false;
+  @ViewChild('objectsAutocomplete') objectsAutocomplete: AutoCompleteComponent;
+  @ViewChild('objectsButton') objectsButton: ElementRef;
 
   public tfms: Tfm[];
   public selectedTfms: any[] = [];
   public showTfms: boolean = false;
+  @ViewChild('tfmsAutocomplete') tfmsAutocomplete: AutoCompleteComponent;
+  @ViewChild('tfmsButton') tfmsButton: ElementRef;
 
   constructor() {
     this.types = [new Type("Bulding"), new Type("Floor"), new Type("Room")];
     this.objects = [new Object("ICB"), new Object("Niproruda"), new Object("Floor1")];
     this.tfms = [new Tfm("Bulding", "221"), new Tfm("Floor", "21"), new Tfm("Computer", "3")];
   }
-
-  @ViewChild('typesCombobox') typesCombobox: ComboBoxComponent;
-
-  @ViewChild('objectsAutocomplete') objectsAutocomplete: AutoCompleteComponent;
 
   public onObjectsAutocompleteValueChange(value: any) {
     if (this.selectedObjects.length == 0) {
@@ -41,8 +42,6 @@ export class StructureComponent {
     }
   }
 
-  @ViewChild('tfmsAutocomplete') tfmsAutocomplete: AutoCompleteComponent;
-
   public onTfmsAutocompleteValueChange(value: any) {
     if (this.selectedTfms.length == 0) {
       this.selectedTfms.push(value);
@@ -51,12 +50,6 @@ export class StructureComponent {
       this.selectedTfms.push(value);
     }
   }
-
-  @ViewChild('objectsButton') objectsButton: ElementRef;
-  @ViewChild('objectsPopup', { read: ElementRef }) public objectsPopup: ElementRef;
-
-  @ViewChild('tfmsButton') tfmsButton: ElementRef;
-  @ViewChild('tfmsPopup', { read: ElementRef }) public tfmsPopup: ElementRef;
 
   @HostListener('keydown', ['$event'])
   public keydown(event: any): void {
@@ -86,18 +79,18 @@ export class StructureComponent {
   }
 
   private containsTfms(target: any): boolean {
-    return this.tfmsButton.nativeElement.contains(target) ||
-      (this.tfmsPopup ? this.tfmsPopup.nativeElement.contains(target) : false);
+    return this.tfmsButton.nativeElement.contains(target);
   }
 
   private containsObjects(target: any): boolean {
-    return this.objectsButton.nativeElement.contains(target) ||
-      (this.objectsPopup ? this.objectsPopup.nativeElement.contains(target) : false);
+    return this.objectsButton.nativeElement.contains(target);
   }
 
   public onClearClicked() {
     this.objectsAutocomplete.reset();
+    this.selectedObjects.pop();
     this.typesCombobox.reset();
     this.tfmsAutocomplete.reset();
+    this.selectedTfms.pop();
   }
 }
